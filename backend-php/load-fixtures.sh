5ed7598511af8211193b33bd717387daae1dc78a
Włodzimierz Gajda <wlodzimierz.gajda@aiqa.tech>
2019-09-20 12:09:43 +0200

    reload fixtures

--- backend-php/load-fixtures.sh
+++ backend-php/load-fixtures.sh
@@ -2,6 +2,6 @@
 
 cd /app/backend-php
 
-#php bin/console fixture:load:city
-#php bin/console fixture:load:mountain
-#php bin/console fixture:load:river
+php bin/console fixture:load:city
+php bin/console fixture:load:mountain
+php bin/console fixture:load:river
